8584f744cf14d4c3bd29b790ae921bfd4a1adf31
fatl4 <altaffar16@gmail.com>
2026-08-10 20:36:39 +1000

    update navbar dan halaman public

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,14 @@
-export default function PublicLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+import Navbar from "@/components/Navbar";
+
+export default function PublicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
 }
